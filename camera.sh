03408d0312bd0c1447123ebcd79ac53521f820c9
Megha Jaiswal <37927368+meghajais11@users.noreply.github.com>
2019-04-18 17:40:33 +0530

    Delete camera.sh

--- camera.sh
+++
@@ -1,4 +0,0 @@
-echo "Camera is starting"
-echo "Open VLC player to live stream"
-raspivid -o - -t 0 -hf -w 640 -h 360 -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264
-echo "Closing Live Stream"
